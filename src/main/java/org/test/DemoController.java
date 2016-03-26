4a15b75fa4a702ef3e269cdad610827b29125846
Yogesh Yadav <yogyadav@cisco.com>
2016-03-26 16:03:06 -0400

    v1

--- src/main/java/org/test/DemoController.java
+++ src/main/java/org/test/DemoController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DemoController {
 	@RequestMapping("/")
 	public String index() {
-		return "I'm up!";
+		return "Hello ! I'm up!";
 	}
 }
